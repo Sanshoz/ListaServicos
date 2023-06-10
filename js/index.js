f3a96cd306bb7358e6a8dcf33e5aae61a05444ea
Matheus <matheusfontenele979700@gmail.com>
2023-06-10 16:42:31 -0300

    depil

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const home = document.querySelector('#container1');
+const servs = document.querySelector('#container2');
+const btnList = document.querySelector('#btn-list');
+
+function showCad(){
+    alert('uiuiu')
+}
+
+btnList.addEventListener('click', () => {   
+    showCad();
+})
